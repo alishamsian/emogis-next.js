ed9f685bf0d1081260087c702740bb854b40d88e
Ali Shamsian <Alishamsian504@gmail.com>
2022-04-20 00:28:05 +0430

    end category card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import { useState } from "react";
+import CategoryCard from "../components/categoryCard";
 import EmojiCard from "../components/EmojiCard";
 import Layout from "../components/Layout";
 import List from "../components/List";
@@ -9,6 +10,14 @@ const Home: NextPage = () => {
   const [filteredEmojis, setFilteredEmojis] = useState(
     emojisData.slice(0, 100)
   );
+  
+  const handleFilterByCategory = (category: string) => {
+    setFilteredEmojis(
+      emojisData.filter((emoji) =>
+      emoji.group.toUpperCase().includes(category.toUpperCase().trim())
+      )
+    )
+  }
 
   const handleFilterSearch = (search: string) => {
     if (search.trim().length > 0) {
@@ -27,6 +36,7 @@ const Home: NextPage = () => {
       <h1 className="text-2xl md:text-4xl font-bold text-gray-600 text-center tracking-wide">
         خوش اومدین به اموجی لند
       </h1>
+      <div className="flex items-center justify-center">
       <input
         className="w-full max-w-xl border border-gray-300 p-3 rounded-md my-8 bg-white shadow-md focus:outline-none focus:border-cyan-500 focus:ring-1"
         type={"text"}
@@ -34,6 +44,55 @@ const Home: NextPage = () => {
         placeholder="اموجی موردنظر را سرچ کنید"
         onChange={(e) => handleFilterSearch(e.target.value)}
       />
+      </div>
+      
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-3 mb-8">
+      <CategoryCard
+              handleFilter={handleFilterByCategory}
+              emoji="😀"
+              group={'شکلک ها و احساسات'}
+            />
+            <CategoryCard
+              handleFilter={handleFilterByCategory}
+              emoji="👨🏻"
+              group={'مردم و اعضای بدن'}
+            />
+            <CategoryCard
+              handleFilter={handleFilterByCategory}
+              emoji="🐶"
+              group={'حیوانات و طبیعت'}
+            />
+            <CategoryCard
+              handleFilter={handleFilterByCategory}
+              emoji="🍔"
+              group={'غذا و نوشیدنی'}
+            />
+            <CategoryCard
+              handleFilter={handleFilterByCategory}
+              emoji="⚽️"
+              group="فعالیت ها"
+            />
+            <CategoryCard
+              handleFilter={handleFilterByCategory}
+              emoji="🏨"
+              group={'سفر و مکان ها'}
+            />
+            <CategoryCard
+              handleFilter={handleFilterByCategory}
+              emoji="💡"
+              group="اشیاء"
+            />
+            <CategoryCard
+              handleFilter={handleFilterByCategory}
+              emoji="⚛️"
+              group="نمادها"
+            />
+            <CategoryCard
+              handleFilter={handleFilterByCategory}
+              emoji="🚩"
+              group="پرچم ها"
+            />
+      </div>
       <List emojis={filteredEmojis.slice(0, 100)} />
     </Layout>
   );
